b4f33d3659b0403e42a8e0f23d23188bf6ecd898
Håkon Malmedal <hmalmedal@gmail.com>
2015-06-15 20:09:51 +0200

    Remove dependencies

--- R/eurovisionsongcontest-package.R
+++ R/eurovisionsongcontest-package.R
@@ -4,21 +4,4 @@
 #'
 #' @name eurovisionsongcontest
 #' @docType package
-#' @importFrom magrittr %>%
 "eurovisionsongcontest"
-
-globalVariables(c("key", "value", "Participant", "Event", "Date"))
-
-.req <- function(pkg) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    stop(sQuote(pkg), " needed. Please install it.", call. = FALSE)
-  }
-}
-
-.reqs <- function() {
-  .req("rvest")
-  .req("dplyr")
-  .req("stringr")
-  .req("tidyr")
-  .req("lubridate")
-}
--- data-raw/data.R
+++ data-raw/data.R
@@ -1,7 +1,32 @@
+scrape <- function(event_url, sleep = 0) {
+  Sys.sleep(sleep)
+
+  event_html <- rvest::html(event_url)
+  event_title <- rvest::html_node(event_html, "title") %>%
+    rvest::html_text() %>%
+    stringr::str_extract("^.+?(?= \\|)")
+  event_date <- rvest::html_node(event_html, ".info:nth-child(2)") %>%
+    rvest::html_text() %>%
+    lubridate::dmy(locale = "C") %>%
+    as.Date()
+  scoreboard_node <- rvest::html_nodes(event_html, "table") %>%
+    getElement(2)
+  scoreboard_header <- rvest::html_nodes(scoreboard_node, "th") %>%
+    rvest::html_nodes("img") %>%
+    rvest::html_attr("title")
+  scoreboard <- rvest::html_table(scoreboard_node)
+  i <- which(names(scoreboard) == "")
+  names(scoreboard)[i] <- scoreboard_header
+  scoreboard$Points <- suppressWarnings(as.integer(scoreboard$Points))
+  scoreboard[is.na(scoreboard)] <- 0
+  scoreboard %>%
+    dplyr::tbl_df() %>%
+    dplyr::mutate(Event = event_title, Date = event_date) %>%
+    tidyr::gather(key, value, -Participant, -Event, -Date)
+}
+
 library(dplyr)
 library(stringr)
-
-devtools::load_all()
 
 base_url <- "http://www.eurovision.tv/page/history/by-year/contest?event="
 events <- c(
